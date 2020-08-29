a18b962416f245e9763c4cdbac4187b756bee447
Belle Sugi Zafimarovololona <sugi.zaf@onja.org>
2020-08-29 11:06:30 +0300

    html, style and generate the film from the link

---
+++ scripts.js
@@ -0,0 +1,45 @@
+// The link for the movie
+
+const baseInfoUrl = "https://ghibliapi.herokuapp.com/films";
+const content = document.querySelector('.movies');
+const movieInfo = document.querySelector('.more_info_movie');
+
+// Feach function for feaching the info
+
+async function fetchMovies() {
+  const response = await fetch(baseInfoUrl);
+  const movies = await response.json();
+  console.log(movies);
+  displayMovies(movies);
+  return movies;
+};
+
+
+// Fuction display movie
+
+async function displayMovies(movies) {
+    const html = movies.map(movie => {
+        return `
+            <div class="movie-detail">
+              <div class="information">
+                <h2>${movie.title}</h2>
+                <div class="movie_data_release">${movie.release_date}</div>
+                <div class="movie_tr_score">${movie.rt_score}</div>
+                <p>${movie.description}</p>
+                <ul class= "list_of_responsible">
+                    <li>
+                    ${movie.director}
+                    </li>
+                    <li>
+                    ${movie.producer}
+                    </li>
+                </ul>
+              </div>
+            </div>`;
+    }).join('');
+
+    movieInfo.innerHTML = html;
+}
+
+fetchMovies();
+
